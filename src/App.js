008e2f42ad986e0d68dfeb7a6d71d97564c910e1
Jaime-Ginesky <jaimelee79@gmail.com>
2022-08-05 22:44:15 -0700

    added Bootstrap and added API robohash

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from  "react";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img alt="robots" src="https://robohash.org/test?set=set4" class=".img-fluid" />
+      <div>
+        <h2>Jane Doe</h2>
+        <p>jane.doe@gmail.com</p>
+      </div>
     </div>
   );
 }
