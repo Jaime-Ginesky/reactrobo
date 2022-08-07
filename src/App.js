3ed7b98d081e86adf11214b13775952314c83b2d
Jaime-Ginesky <jaimelee79@gmail.com>
2022-08-06 19:57:12 -0700

    added grow animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <div className="grow">
       <div className="Card">
       <img alt="robots" src="https://robohash.org/test?size=200x200" class=".img-fluid"  />
       <div className="text-center">
         <h2>Jane Doe</h2>
         <p>jane.doe@gmail.com</p>
+      </div>
       </div>
       </div>
     </div>
