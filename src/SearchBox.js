287cfb78e1052238f14f4931fada38ac928a9441
Jaime-Ginesky <jaimelee79@gmail.com>
2022-08-07 17:50:39 -0700

    created SearchBox component

---
+++ src/SearchBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./SearchBox.css";
+
+const SearchBox = () => {
+   return(
+    <div className="searchBox">
+    <input type="search" 
+    placeholder="search robots" />
+    </div>
+   )
+}
+
+export default SearchBox;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import { robots } from "./robots";
-import CardList from "./CardList";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <CardList robots={ robots }/>
+   <App />
   </React.StrictMode>
 );
 
