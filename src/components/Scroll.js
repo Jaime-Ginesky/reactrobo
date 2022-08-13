f2251a1d2e5f8ea0981fee151b5919961acd380a
Jaime-Ginesky <jaimelee79@gmail.com>
2022-08-12 22:29:15 -0700

    adjusted styling on scroll

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', border: '5px solid orchid', height: '800px'}}>
+        <div style={{overflowY: 'scroll', border: '3px solid powderblue', height: '800px'}}>
             {props.children}
         </div>
     )
